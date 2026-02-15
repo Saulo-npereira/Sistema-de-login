126529962f16b7e250eeaa98ffd382968f914932
saulo-npereira <saulopereira1811@gmail.com>
2026-02-15 19:39:12 -0300

    adicionei a opção de listar seu usuario

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import csv
 import pandas as pd
 import hashlib
 import os
+
 while True:
     print("""
     1 - Cadastrar usuário
     2 - Listar usuários
     3 - Login
-    4 - Sair
+    4 - Listar seu usuário
+    5 - Sair
+    
     """)
 
     while True:
         try:
-            op = int(input('digite um número entre 1 e 4: '))
-            if op>4 or op<1:
+            op = int(input('digite um número entre 1 e 5: '))
+            if op>5 or op<1:
                 raise ValueError
             break
         except ValueError as erro:
@@ -33,8 +36,9 @@ while True:
         with open('arquivo.csv', 'a', newline = '', encoding = 'utf-8') as arquivo:
             writer = csv.writer(arquivo)
             writer.writerow([nome, email, senha])
-
+        os.system('cls')
     if op == 2:
+        os.system('cls')
         tabela = pd.read_csv('arquivo.csv')
         print(tabela)
 
@@ -48,7 +52,7 @@ while True:
             reader = csv.reader(arquivo)
     
             next(reader)  # pula o cabeçalho
-    
+            os.system('cls')
             for linha in reader:
                 if email_login == linha[1] and nome_login == linha[0] and senha_login ==linha[2]:
                     print('usuario logado')
@@ -60,4 +64,27 @@ while True:
                 print('Dados incorretos')
 
     if op==4:
+        usuario_encontrado = False
+        nome_login = str(input('digite seu nome: '))
+        email_login = str(input('digite seu email: '))
+        senha_login = str(input('digite sua senha: '))
+        senha_login = hashlib.sha256(senha_login.encode()).hexdigest()
+        with open('arquivo.csv', 'r', encoding='utf-8') as arquivo:
+            reader = csv.reader(arquivo)
+    
+            next(reader)  # pula o cabeçalho
+            os.system('cls')
+    
+            for linha in reader:
+                if email_login == linha[1] and nome_login == linha[0] and senha_login ==linha[2]:
+                    print(linha)
+                    usuario_encontrado = True
+                    break
+                else:
+                    continue
+                
+            if not usuario_encontrado:
+                print('Dados incorretos')
+
+    if op == 5:
         break
