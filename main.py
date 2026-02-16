4d7be1aff26a257177ce64f2d8c8f7140b0550a7
saulo-npereira <saulopereira1811@gmail.com>
2026-02-15 21:13:47 -0300

    adicionei um banco de dados ao projeto

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import csv
-import pandas as pd
 import hashlib
 import os
+import sqlite3
+
+conexao = sqlite3.connect('usuarios.db')
+cursor = conexao.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS usuario (
+               nome VARCHAR(100),
+               email VARCHAR(100) PRIMARY KEY,
+               senha VARCHAR(100)
+               )''')
 
 while True:
     print("""
     1 - Cadastrar usuário
-    2 - Listar usuários
+    2 - Listar seu usuário
     3 - Login
-    4 - Listar seu usuário
+    4 - Deletar Usuário
     5 - Sair
     
     """)
@@ -27,64 +35,66 @@ while True:
         senha = str(input('digite sua senha: '))
         senha = hashlib.sha256(senha.encode()).hexdigest()
         try:
-            with open('arquivo.csv', 'x', newline='', encoding='utf-8') as arquivo:
-                writer = csv.writer(arquivo)
-                writer.writerow(['nome', 'email', 'senha'])
-        except FileExistsError:
-            pass 
+            cursor.execute('INSERT INTO usuario (nome, email, senha) VALUES(?, ?, ?)', (nome, email, senha))
+            conexao.commit()
+            os.system('cls')
+            print('Usuário cadastrado')
+        except sqlite3.IntegrityError:
+            os.system('cls')
+            print('já existe um usuário com esse email, tente novamente')
 
-        with open('arquivo.csv', 'a', newline = '', encoding = 'utf-8') as arquivo:
-            writer = csv.writer(arquivo)
-            writer.writerow([nome, email, senha])
-        os.system('cls')
     if op == 2:
+        nome_login = str(input('digite seu nome: '))
+        email_login = str(input('digite seu email: '))
+        senha_login = str(input('digite sua senha: '))
+        senha_login = hashlib.sha256(senha_login.encode()).hexdigest()
         os.system('cls')
-        tabela = pd.read_csv('arquivo.csv')
-        print(tabela)
+        try:
+            cursor.execute('SELECT * FROM usuario WHERE nome = ? and email = ? and senha = ?', (nome_login, email_login, senha_login))
+            usuario = cursor.fetchone()
+            print(f"nome = {usuario[0]}\nemail = {usuario[1]}\nsenha = {usuario[2]}")
+        except TypeError:
+            print('Usuario não encontrado')
 
-    if op == 3:
+    if op==3:
         usuario_encontrado = False
         nome_login = str(input('digite seu nome: '))
         email_login = str(input('digite seu email: '))
         senha_login = str(input('digite sua senha: '))
         senha_login = hashlib.sha256(senha_login.encode()).hexdigest()
-        with open('arquivo.csv', 'r', encoding='utf-8') as arquivo:
-            reader = csv.reader(arquivo)
-    
-            next(reader)  # pula o cabeçalho
-            os.system('cls')
-            for linha in reader:
-                if email_login == linha[1] and nome_login == linha[0] and senha_login ==linha[2]:
-                    print('usuario logado')
-                    usuario_encontrado = True
-                    break
-                else:
-                    continue
-            if not usuario_encontrado:
-                print('Dados incorretos')
+        
+        try:
+            cursor.execute("SELECT * FROM usuario WHERE nome = ? and email = ? and senha = ?", (nome_login,
+                                                                             email_login, senha_login))
+            usuario = cursor.fetchone()
+            if usuario == None:
+                raise TypeError
+            print('usuario logado')
+        except TypeError:
+            print('Usuario não encontrado')
+        
 
-    if op==4:
-        usuario_encontrado = False
-        nome_login = str(input('digite seu nome: '))
-        email_login = str(input('digite seu email: '))
-        senha_login = str(input('digite sua senha: '))
-        senha_login = hashlib.sha256(senha_login.encode()).hexdigest()
-        with open('arquivo.csv', 'r', encoding='utf-8') as arquivo:
-            reader = csv.reader(arquivo)
+    if op == 4:
+        nome_delete = str(input('digite seu nome: '))
+        email_delete = str(input('digite seu email: '))
+        senha_delete = str(input('digite sua senha: '))
+        senha_delete = hashlib.sha256(senha_delete.encode()).hexdigest()
+        try:
+            cursor.execute("SELECT * FROM usuario WHERE nome = ? and email = ? and senha = ?", (nome_delete,
+                                                                                email_delete, senha_delete))
+            usuario = cursor.fetchone()
+            if usuario == None:
+                raise TypeError
+            else:
+                confirmacao = input(f'Para confirmar, digite: {senha_delete[:8]}: ')
+                if confirmacao == senha_delete[:8]:
+                    cursor.execute('DELETE FROM usuario WHERE email = ?', (email_delete,))
+                    conexao.commit()
+                    print('Usuário deletado')
+        except TypeError:
+            print('Usuario não encontrado')
     
-            next(reader)  # pula o cabeçalho
-            os.system('cls')
-    
-            for linha in reader:
-                if email_login == linha[1] and nome_login == linha[0] and senha_login ==linha[2]:
-                    print(linha)
-                    usuario_encontrado = True
-                    break
-                else:
-                    continue
-                
-            if not usuario_encontrado:
-                print('Dados incorretos')
-
     if op == 5:
+        print('saindo...')
+        conexao.close()
         break
